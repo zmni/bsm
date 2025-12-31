8950ec21ed51b5f590f7dc9494d9d2d98b0f321b
zmni <zmni@users.noreply.github.com>
2025-12-31 18:48:10 +0700

    v2

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://zmni.github.io',
+  base: '/bsm'
+});
